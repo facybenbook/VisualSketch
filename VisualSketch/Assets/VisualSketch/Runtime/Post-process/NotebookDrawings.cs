8f0b43542e348114a2ab6bd5514efed43484393a
云却 <479813005@qq.com>
2019-03-18 23:00:12 +0900

    搭建目录结构

---
+++ VisualSketch/Assets/VisualSketch/Runtime/Post-process/NotebookDrawings.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Rendering;
+
+namespace UnityEngine.Experimental.Rendering.HDPipeline
+{
+    [Serializable, VolumeComponentMenu("Post-processing/NotebookDrawings")]
+    public class NotebookDrawings : VolumeComponent
+    {
+        
+    }
+}
